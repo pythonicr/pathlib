a0a6f4d665f993bf42e58755791ee10b93edf819
Garrett Shipley <garrett.shipley7+github@gmail.com>
2023-12-26 16:08:38 -0500

    Update utils.R

--- R/utils.R
+++ R/utils.R
@@ -1,21 +1,18 @@
 .is_absolute <- function(paths, invert = FALSE) {
-  if (isTRUE(invert)) {
-    !base::grep("^(/+|[A-Za-z]:)", .tidy(paths))
-  } else {
-    base::grep("^(/+|[A-Za-z]:)", .tidy(paths))
-  }
+  out <- base::grepl("^(/+|[A-Za-z]:)", .tidy(paths))
+  if (isTRUE(invert)) !out else out
 }
 
 .test_type <- function(paths, type_name) {
   type_id <- fs:::file_types[[type_name]]
   # %in% treats NAs as valid objects, so comparisons don't pass NAs
   # along
-  return(base::.Call("fs_stat_", paths, TRUE, PACKAGE = "fs")$type %in% type_id)
+  base::.Call("fs_stat_", paths, TRUE, PACKAGE = "fs")$type %in% type_id
 }
 
 .tidy <- function(paths) {
   if (!base::inherits(paths, "fs_path")) {
     paths <- fs::path_tidy(paths)
   }
-  return(paths)
+  paths
 }
